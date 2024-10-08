9c9aaf1fc2e1fde13da3c34030020d5d6dff3e22
Sudhanshu061 <140046002+Sudhanshu061@users.noreply.github.com>
2024-10-08 15:48:01 +0530

    Assessment

--- program1.py
+++ program1.py
@@ -5,7 +5,21 @@ class Solution(object):
         :rtype: bool
         """
         pass
+class Solution(object):
+    def isValid(self, s):
+        stack = [] # only use append and pop
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+                return False
 
+        return len(stack) == 0
 
 
 
